af8a4b680196f9d8584dadc85c029514e66123e9
Ernesto <ernesto.scorzelli77@gmail.com>
2026-04-24 16:54:55 +0200

    Security

--- the_last_dance/src/main/java/ernesto/the_last_dance/security/SecurityConfig.java
+++ the_last_dance/src/main/java/ernesto/the_last_dance/security/SecurityConfig.java
@@ -3,12 +3,27 @@ package ernesto.the_last_dance.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) {
+
+        httpSecurity.formLogin(form -> form.disable());
+        httpSecurity.csrf(csrf -> csrf.disable());
+        httpSecurity.sessionManagement(sessions -> sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.authorizeHttpRequests(req -> req.requestMatchers("/**").permitAll());
+
+        return httpSecurity.build();
+    }
+
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder(12);
